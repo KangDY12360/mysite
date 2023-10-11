2d9cc587c5c369da3094fcef0436b2ee7d193aeb
kdy <tutu4443@naver.com>
2023-10-11 23:36:16 +0900

    1011

--- polls/models.py
+++ polls/models.py
@@ -17,3 +17,7 @@ class Choice(models.Model):
     vote=models.IntegerField(default=0)
     def __str__(self):
         return self.question_text
+    
+def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
